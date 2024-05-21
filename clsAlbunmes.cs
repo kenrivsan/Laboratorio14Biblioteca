c9b78417131264f70f9e33d3b738081c241f8355
kenri <kennethriv@umes.edu.gt>
2024-05-20 20:39:18 -0600

    Agregar archivos de proyecto.

---
+++ clsAlbunmes.cs
@@ -0,0 +1,15 @@
+﻿namespace Laboratorio14Biblioteca
+{
+    public class clsAlbunmes : clsCanciones
+    {
+        public string Titulo { get; set; }
+        public string listadoCanciones { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public clsAlbunmes()
+        {
+            Titulo = string.Empty;
+            listadoCanciones = string.Empty;
+            FechaPublicacion = DateTime.Now;
+        }
+    }
+}
---
+++ clsCanciones.cs
@@ -0,0 +1,17 @@
+﻿namespace Laboratorio14Biblioteca
+{
+    public class clsCanciones
+    {
+        public string Nombre {get; set;}
+        public string Artista { get; set;}
+        public decimal Duracion { get; set;}
+
+        public clsCanciones()
+        {
+            Nombre = string.Empty;
+            Artista = string.Empty;
+            Duracion = 0;
+        }
+
+    }
+}
